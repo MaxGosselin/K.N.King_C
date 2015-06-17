5ea904ec15450184d8a7d8a8a8aed9876704b17a
Max gosselin <Max.gosselin@mail.utoronto.ca>
2015-06-17 04:52:20 +0200

    Read printf section chapt 3 of KNKC today, did the first exercise; will continue tomorrow.

---
+++ Code/3--Formatted_IO/3.1.c
@@ -0,0 +1,30 @@
+/*
+ *
+ * Exercise 1:
+ *
+ *      What output do the following calls of printf produce?
+ *
+ */
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\n I predict :\n     86,1040\n");
+    printf("%6d,%4d\n", 86, 1040);
+
+    printf("I predict:\n 3.02530e+01\n");
+    printf("%12.5e\n", 30.253);
+
+    printf("I predict:\n 83.1620\n");
+    printf("%.4f\n", 83.162);
+
+    printf("I predict:\n 9.9e-8\n");
+    printf("%-6.2g\n", .0000009979);
+
+    printf("\n\nHuh! Very interesting... Science #s stay away plis :D");
+    
+    return 0;
+}
+
