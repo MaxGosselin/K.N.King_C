a6e48d22d1cab8968d765b51523ff7220036f695
Max gosselin <Max.gosselin@mail.utoronto.ca>
2015-07-14 03:40:48 +0200

    Did some work on the Expressions exercises after reading the chapter on the dock. aaahhhh.

---
+++ Code/4--Expressions/4.2.c
@@ -0,0 +1,15 @@
+/* Chapt 4 Exercises
+ *
+ */
+
+#include<stdio.h>
+
+int main(void)
+{
+    printf("%d %d\nThis is proof that in C99 (-i)/j == - (i/j)."
+            " In C89, however, this would perhaps produce errors.\n", (-9)/7, \
+            -(9/7));
+
+    return 0;
+
+}
