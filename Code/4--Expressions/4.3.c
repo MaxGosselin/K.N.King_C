872795415443ca3d46cc2a04b94d370aefc95674
MaxGosselin <Max.jp.Gosselin@gmail.com>
2015-07-16 07:15:29 +0200

    Done for tonight, still don't really understand why negative int division and remainder fuck things up so much...

---
+++ Code/4--Expressions/4.3.c
@@ -0,0 +1,17 @@
+/* Chapt 4 Exercises 
+ *
+ * What is the value of each of the following expressions in C?
+ */
+
+#include<stdio.h>
+
+int main(void)
+{
+    printf("%d\n", 8 / 5);
+    printf("%d\n", -8 / 5);
+    printf("%d\n", 8 / -5);
+    printf("%d\n", -8 / -5);
+
+    return 0;
+}
+
